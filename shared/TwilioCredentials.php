e4b22312d1458f0f134ecff686867b67ba95e0d1
Danny Brown <csengineer13@gmail.com>
2016-01-21 21:04:28 -0500

    Delete TwilioCredentials.php

--- shared/TwilioCredentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// AccountSid and AuthToken from www.twilio.com/user/account
-$TwilioAccountSid = "ACef24392a556d43a680befa872e5d5469";
-$TwilioAuthToken = "edc604603604a22ff3609ec298c24d0e";
-$TWILIONUMBER = "+14196794816";
